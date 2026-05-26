36b43fc5c225dd4831c00921db4377c2b1c08ce1
deveshmundharikar <rooknighty@gmail.com>
2026-05-27 01:08:21 +0530

    Add parameterized Helm chart, templates, and production value overrides

--- demo-k8s-project/backend/src/server.js
+++ demo-k8s-project/backend/src/server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
